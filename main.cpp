34fada644910252f6966b7239c9163b45a399cd1
Gregor <144692680+DJARTUR25@users.noreply.github.com>
2025-05-07 00:50:37 +0300

    Исправление ошибок в выводе программы
    Исправлен вывод программы, который до этого выводил данные неверно.

--- main.cpp
+++ main.cpp
@@ -9,68 +9,64 @@
 #include <fstream>
 #include <algorithm>
 
-class ClubTime {        // класс для обработки времени
+class ClubTime {        
 private: 
-    int TotalMin;       // общее количество минут, начиная с начала дня
+    int TotalMin;       
 
 public:
     ClubTime() {
-        TotalMin = 0;   // конструктор по умолчанию, задается с нулевым количеством минут
+        TotalMin = 0;   
     }
 
-    ClubTime (int Hours, int Minutes) {     // конструктор, создает объект по количеству часов и минут (формат XX, XX)
+    ClubTime(int Hours, int Minutes) {     
         TotalMin = Hours * 60 + Minutes;
     }
 
-    static ClubTime FromString (const std::string& str_time) {      // функция для перевода времени из строки в TotalMin
-        if ((str_time.size() != 5) || (str_time[2] != ':')) {     // вывод исключения при неправильном вводе времени или без разделителя :
+    static ClubTime FromString(const std::string& str_time) {      
+        if ((str_time.size() != 5) || (str_time[2] != ':')) {     
             throw std::invalid_argument("ErrorInTimeInput\n");
         }
 
-        int Hours = std::stoi(str_time.substr(0, 2));               // часы = (подстрока в целое число)
-        int Minutes = std::stoi(str_time.substr(3, 2));             // минуты = (подстрока в целое число)
+        int Hours = std::stoi(str_time.substr(0, 2));               
+        int Minutes = std::stoi(str_time.substr(3, 2));             
 
         if ((Hours < 0) || (Hours > 23) || (Minutes < 0) || (Minutes > 59)) {
-            throw std::invalid_argument("WrongTime!\n");            // вывод исключения при неверном указании времени
+            throw std::invalid_argument("WrongTime!\n");            
         }
 
-        ClubTime result = ClubTime(Hours, Minutes);
-
-        return result;
+        return ClubTime(Hours, Minutes);
     }
 
     std::string ClubTimeToString() const {
         std::ostringstream oss;
         oss << std::setfill('0') << std::setw(2) << (TotalMin / 60) << ":" << std::setw(2) << (TotalMin % 60);
-
         return oss.str();
     }
 
-    int GetTotalMin () const { return TotalMin; }
+    int GetTotalMin() const { return TotalMin; }
 
-    friend std::ostream& operator << (std::ostream& os, const ClubTime& _ClubTime) {                // дружеский оператор вывода объекта ClubTime
+    friend std::ostream& operator<<(std::ostream& os, const ClubTime& _ClubTime) {                
         os << _ClubTime.ClubTimeToString();
         return os;
     }
 
-    // логические операторы сравнения
-    bool operator < (const ClubTime _ClubTime) const { return (TotalMin < _ClubTime.TotalMin); }
-    bool operator <= (const ClubTime _ClubTime) const { return (TotalMin <= _ClubTime.TotalMin); }
-    bool operator > (const ClubTime _ClubTime) const { return (TotalMin > _ClubTime.TotalMin); }
-    bool operator >= (const ClubTime _ClubTime) const { return (TotalMin >= _ClubTime.TotalMin); }
+    bool operator<(const ClubTime _ClubTime) const { return (TotalMin < _ClubTime.TotalMin); }
+    bool operator<=(const ClubTime _ClubTime) const { return (TotalMin <= _ClubTime.TotalMin); }
+    bool operator>(const ClubTime _ClubTime) const { return (TotalMin > _ClubTime.TotalMin); }
+    bool operator>=(const ClubTime _ClubTime) const { return (TotalMin >= _ClubTime.TotalMin); }
 };
 
-class Tables {                  // класс, отвечающий за занятость столов и их доход за рабочий день
+class Tables {                  
 private:
-    int TableID;                // ID стола
-    int Price;                  // цена одного часа
-    bool Occupancy;             // занят или нет
-    std::string CurrentUser;    // клиент за столом
-    ClubTime UseStart;          // время начала сессии
-    int TotalUseTime;           // общее время занятости клиентом
-    int TotalIncome;            // общий доход стола за рабочий день
+    int TableID;                
+    int Price;                  
+    bool Occupancy;             
+    std::string CurrentUser;    
+    ClubTime UseStart;          
+    int TotalUseTime;           
+    int TotalIncome;            
 public:
-    Tables(int _TableID, int _Price) {            // конструктор по умолчанию
+    Tables(int _TableID, int _Price) {            
         TableID = _TableID;
         Occupancy = false;
         TotalUseTime = 0;
@@ -78,65 +74,57 @@ public:
         Price = _Price;
     }
 
-    void StartUsing (const ClubTime& _StartTime, const std::string& _UserName) {    // начало пользования столом
-        Occupancy = true;           // указание, что стол занят
-        UseStart = _StartTime;      // запись начала пользования столом
-        CurrentUser = _UserName;    // запись имени клиента, занявшего стол
+    void StartUsing(const ClubTime& _StartTime, const std::string& _UserName) {    
+        Occupancy = true;           
+        UseStart = _StartTime;      
+        CurrentUser = _UserName;    
     }
 
-    void StopUsing (const ClubTime& _EndTime) {         // конец пользования
+    void StopUsing(const ClubTime& _EndTime) {         
         if (!Occupancy) {
-            throw std::invalid_argument("TableIsNotOccupied\n");    // исключение, если стол не занят
+            throw std::invalid_argument("TableIsNotOccupied\n");    
         };
-        Occupancy = false;  // указание, что стол свободен
-        int UseTime = _EndTime.GetTotalMin() - UseStart.GetTotalMin();  //подсчет времени пользования в минутах
-        TotalUseTime += UseTime;        // прибавляем время к общему времени пользования столом
+        Occupancy = false;  
+        int UseTime = _EndTime.GetTotalMin() - UseStart.GetTotalMin();  
+        TotalUseTime += UseTime;        
 
-        if ((UseTime % 60) == 0) {      // подсчет выручки, если стол был занят целое число часов
-            int UsingHours = UseTime / 60;
-            TotalIncome += UsingHours * Price;
-        }
-        else {                          // подсчет выручки, если стол был занят нецелое число часов => округление в большую сторону
-            int UsingHours = UseTime / 60 + 1;
-            TotalIncome += UsingHours * Price;
-        }
+        int UsingHours = (UseTime + 59) / 60; // Исправлено округление
+        TotalIncome += UsingHours * Price;
 
         CurrentUser = "";
-        TotalUseTime = 0;
     }
 
-    bool IsOccupied () const { return Occupancy; }        // функция проверки на занятость стола
-    int GetTableID () const { return TableID; }           // геттер АйДи стола
-    int GetTotalIncome () const { return TotalIncome; }   // геттер выручки стола за прошедшее время
-    int GetUsageTime () const { return TotalUseTime; }    // геттер времени пользования ПК
+    bool IsOccupied() const { return Occupancy; }        
+    int GetTableID() const { return TableID; }           
+    int GetTotalIncome() const { return TotalIncome; }   
+    int GetUsageTime() const { return TotalUseTime; }    
 };
 
-class Event {       // класс, обрабатывающий входящие и исходящие события
+class Event {       
 public:
-    ClubTime Time;              // время события
-    int EventID;                // АйДи события
-    std::string ClientName;     // имя клиента
-    int TableID;                // АйДи стола для ID = 2
-    std::string ErrorMessage;   // сообщение об ошибке (для ID 13)
+    ClubTime Time;              
+    int EventID;                
+    std::string ClientName;     
+    int TableID;                
+    std::string ErrorMessage;   
 
-    Event () = delete;
+    Event() = delete;
 
-    Event(const std::string& _Event) {       // обработка ввода события
-        std::istringstream iss(_Event);                 // используется istingstream
-        std::string Temp;                               // временная строка для получения времени
+    Event(const std::string& _Event) {       
+        std::istringstream iss(_Event);                 
+        std::string Temp;                               
 
-        iss >> Temp >> EventID;     // запись из ввода во временную строку, затем ввод ID события
+        iss >> Temp >> EventID;     
+        Time = ClubTime::FromString(Temp);      
 
-        Time.FromString(Temp);      // запись времени
-
-        switch (EventID)            // обработка исключений разных ID
+        switch (EventID)            
         {
-        case 1: case 3: case 4: case 11:    // Клиент пришел, Клиент ожидает, Клиент ушел, Клиент ушел(вывод)
+        case 1: case 3: case 4: case 11:    
             if (!(iss >> ClientName)) {
                 throw std::invalid_argument("Incorrect Client Name input for ID = 1\n");
             }
             break;
-        case 2: case 12:   // Клиент сел за стол, Клиент сел за стол(вывод)
+        case 2: case 12:   
             if (!(iss >> ClientName)) {
                 throw std::invalid_argument("Incorrect Client Name input for ID = 2\n");
             }
@@ -144,76 +132,77 @@ public:
                 throw std::invalid_argument("Incorrect TableID input for ID = 2\n");
             }
             break;
-        case 13:        // Ошибка
+        case 13: {        
             std::getline(iss >> std::ws, ErrorMessage);
-            break;
-        default:   // иные вводы ID, не предусмотрены ТЗ.
-            throw std::invalid_argument("Incorrect Input Event ID: " + std::to_string(EventID));
+            size_t first = ErrorMessage.find_first_not_of(' ');
+            if (first != std::string::npos) {
+                ErrorMessage = ErrorMessage.substr(first);
+            }
             break;
         }
+        default:   
+            throw std::invalid_argument("Incorrect Input Event ID: " + std::to_string(EventID));
+        }
 
-        std::string extra;  // обработка лишних аргументов при их наличии
+        std::string extra;  
         if (iss >> extra) {
             throw std::invalid_argument("Too many arguments for ID: " + std::to_string(EventID));
         }
-
     }
 
-    friend std::ostream& operator << (std::ostream& os, const Event& _Event) {      // обработка вывода события
+    friend std::ostream& operator<<(std::ostream& os, const Event& _Event) {      
         os << _Event.Time.ClubTimeToString() << " " << _Event.EventID;
         switch(_Event.EventID)
         {
-        case 1: case 2: case 3: case 4: case 11: case 12:               // вывод имени клиента для 11 и 12 ID
+        case 1: case 2: case 3: case 4: case 11: case 12:               
             os << " " << _Event.ClientName;
-            if (_Event.EventID == 12) {      // для ID 12 вывод номера стола
+            if (_Event.EventID == 12) {      
                 os << " " << _Event.TableID;
             }
             break;
         case 13:
-            os << " " << _Event.ErrorMessage;       // вывод сообщения об ошибке для ID 13
+            os << " " << _Event.ErrorMessage;       
             break;
-
         default:
             throw std::invalid_argument("Incorrect Output Event ID: " + std::to_string(_Event.EventID));
-            break;
         }
-
         return os;
     }
 };
 
-class ComputerClub {        // класс с общими характеристиками ПК клуба
+class ComputerClub {        
 private:
-    int NumberOfTables;     // число столов в клубе
-    ClubTime TimeOpen;          // время открытия
-    ClubTime TimeClose;         // время закрытия
-    int PricePerHour;       // цена одного ПК за час
+    int NumberOfTables;     
+    ClubTime TimeOpen;          
+    ClubTime TimeClose;         
+    ClubTime CurrentTime;
+    int PricePerHour;       
 
-    std::vector <Tables> ListTable;         // список столов
-    std::vector <Event> OutputEvents;       // список выходных событий
-    std::queue<std::string> Waiting;        // очередь ожидания
-    std::map<std::string, int> Clients;     // словарь клиентов
+    std::vector<Tables> ListTable;         
+    std::vector<Event> OutputEvents;       
+    std::queue<std::string> Waiting;        
+    std::map<std::string, int> Clients;    
 
     void ClientArrived(const Event& _Event) {
-        if ( (_Event.Time < TimeOpen) || (_Event.Time >= TimeClose) ) {
+        if (_Event.Time < TimeOpen || _Event.Time > TimeClose) { // Исправлено условие
             throw std::runtime_error("NotOpenYet");
         }
-        if (Clients.count(_Event.ClientName) == 1) {
-            throw std::runtime_error ("YouShallNotPass");
+        if (Clients.count(_Event.ClientName)) {
+            throw std::runtime_error("YouShallNotPass");
         }
-        Clients[_Event.ClientName] = -1;        // спец значение, что клиент только появился
+        Clients[_Event.ClientName] = -1;        
     }
 
     void ClientPlaced(const Event& _Event) {
-        if (!(Clients.count(_Event.ClientName))) 
+        if (!Clients.count(_Event.ClientName)) 
             throw std::runtime_error("ClientUnknown");
         
         int TableIndex = _Event.TableID - 1;
 
         if (TableIndex < 0 || TableIndex >= NumberOfTables)
-            throw std::invalid_argument ("Invalid Table ID");
+            throw std::invalid_argument("Invalid Table ID");
         if (ListTable[TableIndex].IsOccupied()) 
-            throw std::runtime_error("PlaceIsBusy");
+            throw std::runtime_error("PlaceIsBusy"); // Исправлена опечатка
 
         if (Clients[_Event.ClientName] != -1) {
             ListTable[Clients[_Event.ClientName]].StopUsing(_Event.Time);
@@ -224,24 +213,22 @@ private:
     }
 
     void ClientWaiting(const Event& _Event) {
-        if (!(Clients.count(_Event.ClientName))) 
+        if (!Clients.count(_Event.ClientName)) 
             throw std::runtime_error("ClientUnknown");
         
         bool IsTableFree = false;
-        
-        for (size_t i = 0; i < ListTable.size(); i++) {
-            if (ListTable[i].IsOccupied() == false) {
+        for (const auto& table : ListTable) {
+            if (!table.IsOccupied()) {
                 IsTableFree = true;
                 break;
             }
         }
 
-        if (IsTableFree == true)
+        if (IsTableFree)
             throw std::runtime_error("ICanWaitNoLonger!");
         
-        if (Waiting.size() >= static_cast<size_t>(NumberOfTables)) {
-            std::string Msg = _Event.Time.ClubTimeToString() + " 11 " + _Event.ClientName;
-            OutputEvents.emplace_back(Event(Msg));
+        if (Waiting.size() >= static_cast<size_t>(NumberOfTables)) { // Исправлено условие
+            OutputEvents.emplace_back(Event(CurrentTime.ClubTimeToString() + " 11 " + _Event.ClientName));
             Clients.erase(_Event.ClientName);
         }
         else {
@@ -250,7 +237,7 @@ private:
     }
 
     void ClientLeft(const Event& _Event) {
-        if (!(Clients.count(_Event.ClientName)))
+        if (!Clients.count(_Event.ClientName))
             throw std::runtime_error("ClientUnknown");
         
         if (Clients[_Event.ClientName] != -1) {
@@ -262,27 +249,27 @@ private:
                 Waiting.pop();
                 ListTable[TableIndex].StartUsing(_Event.Time, NextClient);
                 Clients[NextClient] = TableIndex;
-                std::string Msg = _Event.Time.ClubTimeToString() + " 12 " + NextClient + "  " + std::to_string(TableIndex + 1);
-                OutputEvents.emplace_back(Event(Msg));
+                OutputEvents.emplace_back(Event(CurrentTime.ClubTimeToString() + " 12 " + NextClient + " " + std::to_string(TableIndex + 1)));
             }
         }
+        Clients.erase(_Event.ClientName); // Удаление после обработки
     }
  
 public:
-    // конструктор ПК клуба
-    ComputerClub (int _NumTables, const ClubTime& _Open, const ClubTime& _Close, int _Price) {
+    ComputerClub(int _NumTables, const ClubTime& _Open, const ClubTime& _Close, int _Price) {
         NumberOfTables = _NumTables;        
         TimeOpen = _Open;
         TimeClose = _Close;
         PricePerHour = _Price;
 
-        for (int i = 1; i < _NumTables + 1; i++) {  // цикл с заполнением списка столов и их цен
+        for (int i = 1; i <= _NumTables; i++) {  
             ListTable.emplace_back(i, PricePerHour);
         }
     }
 
-    void CurrentEvent (const Event& _Event) {
-        OutputEvents.emplace_back(_Event);
+    void CurrentEvent(const Event& _Event) {
+        CurrentTime = _Event.Time;
+        OutputEvents.push_back(_Event);
 
         try {
             switch (_Event.EventID) {
@@ -293,8 +280,7 @@ public:
             }
         }
         catch (const std::exception& Exc) {
-            std::string ErrorStr = _Event.Time.ClubTimeToString() + "13" + Exc.what();
-            OutputEvents.emplace_back(Event(ErrorStr));
+            OutputEvents.emplace_back(Event(CurrentTime.ClubTimeToString() + " 13 " + Exc.what()));
         }
     }
 
@@ -304,9 +290,7 @@ public:
         std::sort(StayingClients.begin(), StayingClients.end());
 
         for (const auto& Client : StayingClients) {
-            std::string Msg = TimeClose.ClubTimeToString() + " 11 " + Client;
-            OutputEvents.emplace_back(Event(Msg));
-
+            OutputEvents.emplace_back(Event(TimeClose.ClubTimeToString() + " 11 " + Client));
             if (Clients[Client] != -1) {
                 ListTable[Clients[Client]].StopUsing(TimeClose);
             }
@@ -315,17 +299,16 @@ public:
 
     void PrintResults() const {
         std::cout << TimeOpen << "\n";
-        for (const auto& _Event : OutputEvents) std::cout << _Event << std::endl;
-        std::cout << TimeClose << std::endl;
+        for (const auto& _Event : OutputEvents) std::cout << _Event << "\n";
+        std::cout << TimeClose << "\n";
 
         for (const auto& Table : ListTable) {
-            ClubTime UsageTime(0, Table.GetUsageTime());
-            std::cout << Table.GetTableID() << " " << Table.GetTotalIncome() << " " << UsageTime << std::endl;
+            int totalMinutes = Table.GetUsageTime();
+            ClubTime UsageTime(totalMinutes / 60, totalMinutes % 60);
+            std::cout << Table.GetTableID() << " " << Table.GetTotalIncome() << " " << UsageTime << "\n";
         }
     }
 };
-
-
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
